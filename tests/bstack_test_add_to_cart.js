c373b36eb3aa319eebe565ab4f7ec59bee76330c
mandarkulkarni13 <mandar.k@browserstack.com>
2025-08-12 20:41:30 +0530

    Update bstack_test_add_to_cart.js

--- tests/bstack_test_add_to_cart.js
+++ tests/bstack_test_add_to_cart.js
@@ -4,13 +4,13 @@ const { percyScreenshot } = require("@percy/playwright");
 test('BStackDemo test add to cart', async ({ page }) => {
   // visit the site
   await page.goto('https://bstackdemo.com/');
-  await percyScreenshot(page, "Screenshot 1");
+  
   // get name of product we want to add to cart\
   const productToAdd = await page.locator('#\\33  > p').textContent();
 
   // click on add to cart
   await page.click('#\\33 > .shelf-item__buy-btn');
-
+  await percyScreenshot(page, "Screenshot 1");
   // get name of item in cart
   const productInCart = await page.textContent('#__next > div > div > div.float-cart.float-cart--open > div.float-cart__content > div.float-cart__shelf-container > div > div.shelf-item__details > p.title');
   
